5d4aede3da90de094d4f46d64f20315a357bf225
Kai Shi <shi.kai@outlook.com>
2022-02-27 23:50:33 -0500

    create news hub app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Navbar, Container, Nav, Card, Button } from "react-bootstrap";
 
-function App() {
+const CategoryList = [
+  "all",
+  "national",
+  "business",
+  "sports",
+  "world",
+  "politics",
+  "technology",
+  "startup",
+  "entertainment",
+  "miscellaneous",
+  "hatke",
+  "science",
+  "automobile",
+];
+
+const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export default function App() {
+  const [category, setCategory] = useState(CategoryList[0]);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://inshortsapi.vercel.app/news?category=${category}`)
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, [category]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand>NewsHub</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              {CategoryList.map((item) => {
+                return (
+                  <Nav.Link key={item} onClick={() => setCategory(item)}>
+                    {capitalizeFirstLetter(item)}
+                  </Nav.Link>
+                );
+              })}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {Object.keys(data).length > 0 && (
+        <div className="mx-auto" style={{ width: "24rem" }}>
+          <h1>{capitalizeFirstLetter(data.category)}</h1>
+          {data.data.map((item) => {
+            return (
+              <Card key={item.title}>
+                <Card.Img variant="top" src={item.imageUrl} />
+                <Card.Body>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Text>{item.content}</Card.Text>
+                  <div className="d-flex justify-content-end">
+                    <Button
+                      variant="light"
+                      size="sm"
+                      href={item.readMoreUrl}
+                      target="_blank"
+                    >
+                      Read more
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
